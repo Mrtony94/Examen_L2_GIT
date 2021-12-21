b23b29172de83c9ab2be6618a72729e4b832dca7
mrtony94 <tony.mst94@gmail.com>
2021-12-21 11:00:08 +0100

    protocols

--- piedrapapeltijerahafsa/protocol.py
+++ piedrapapeltijerahafsa/protocol.py
@@ -1,12 +1,27 @@
-MSG_JOIN = "JOIN"
-MSG_SERVER = "SERVER"
-GET_ANSWER = "GET_ANSWER"
-SEND_COMMAND = "COMMAND"
-END_GAME = "END"
-GAME_FULL = "FULL"
-
-import struct
 import json
+import struct
+
+JOIN = "JOIN"
+WELCOME = "WELCOME"
+LOAD_GAME = "LOAD_GAME"
+LOAD_GAME_ANSWER = "LOAD_GAME_ANSWER"
+SERVER_OPTION = "SEND_SERVER_OPTION"
+CHOOSE_CHARACTER = "CHOOSE_CHARACTER"
+CHARACTER = "SEND_CHARACTER"
+SERVER_MESSAGE = "SERVER_MESSAGE"
+YOUR_TURN = "YOUR_TURN"
+CHARACTER_COMMAND = "SEND_CHARACTER_COMMAND"
+GAMES = "SEND_GAMES"
+GAME_CHOICE = "SEND_GAME_CHOICE"
+VALID_GAME = "SEND_VALID_GAME"
+END_GAME = "END_GAME"
+DC_ME = "DC_ME"
+DC_SERVER = "DC_SERVER"
+
+
+class ConnectionClosed(Exception):
+    def __init__(self):
+        super().__init__("Connection closed by other")
 
 
 def recv_all(recipient, length):
@@ -27,12 +42,14 @@ def send_one_message(recipient, message):
     recipient.sendall(header)
     recipient.sendall(encoded_data)
 
+
 def recv_one_message(recipient):
     header_buffer = recv_all(recipient, 4)  # int size -> 4
-    if header_buffer is None:
-        print("nada")
-    else:
+    if header_buffer:
         header = struct.unpack("!I", header_buffer)
         length = header[0]
-        data_buffer = recv_all(recipient, length)
-        return json.loads(data_buffer.decode())
+        encoded_data = recv_all(recipient, length)
+        message = json.loads(encoded_data.decode())
+        return message
+    else:
+        raise ConnectionClosed()
